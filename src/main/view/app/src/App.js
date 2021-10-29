c5eb6213f71cce027ff3a2aa579657266bb7dff3
lmwamba-hai <bmwamba@stedwards.edu>
2021-10-28 23:04:46 -0500

    fixed app.js after merger

--- src/main/view/app/src/App.js
+++ src/main/view/app/src/App.js
@@ -1,35 +1,3 @@
-import './App.css';
-import { Component } from 'react';
-<<<<<<< HEAD
-import Login from './Pages/Login';
-import { Redirect, Route } from "react-router-dom";
-import axios from "axios";
-
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      user: {},
-  }
-
-  login = async (loginObj) => {
-    const response = await axios.post("/", loginObj);
-    console.log(response.data.data);
-    this.setState({ user: response.data.data }, () => {
-      history.push("/");
-    });
-  };
-
-  render()
-  {
-    return(
-      <div>
-        <Route exact path = '/' Component = {Login} ></Route>
-      </div> 
-    );
-  }
-}}
-=======
 import Login from './Pages/Login/Login';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import axios from "axios";
@@ -49,30 +17,5 @@ function App(){
         </div>
     )
 }
-// class App extends Component {
-//   constructor(){
-//     super();
-//     this.state = {
-//       user: {},
-//   }
-
-//   login = async (loginObj) => {
-//     const response = await axios.post("/", loginObj);
-//     console.log(response.data.data);
-//     this.setState({ user: response.data.data }, () => {
-//       history.push("/");
-//     });
-//   };
-
-//   render()
-//   {
-//     return(
-//       <div>
-//         <Route exact path = '/' Component = {Login} ></Route>
-//       </div> 
-//     );
-//   }
-// }}
->>>>>>> origin/frontend
 
 export default App;

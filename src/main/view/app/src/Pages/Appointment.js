fbf093934c53f7ee2b5de0bf386a585cd312cbec
lmwamba-hai <bmwamba@stedwards.edu>
2021-10-31 13:45:46 -0500

    delete extra login.js

--- src/main/view/app/src/Pages/Appointment.js
+++ src/main/view/app/src/Pages/Appointment.js
@@ -3,8 +3,21 @@ import React from "react";
 import NavBar from "../Componets/NavBar/NavBar";
 
 
+
 class Appointment extends React.Component{
-    
+
+    getAllPatientAppointments() {
+        console.log(this.state)
+        axios.post("http://localhost:8080/patient/login/", this.state)
+            .then(function (response) {
+                console.log(this)
+                console.log("this is the response", response.data);
+            })
+            .catch(function (error) {
+                console.log("this is the error", error);
+            });
+    }
+
     render(){
         return(
             <div className = "Appointment">
